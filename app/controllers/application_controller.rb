a0a99337e9a1be32f467a4d5d69102a6036fc77c
yuda12c <ngsrn.910@gmail.com>
2024-12-23 17:36:31 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-<<<<<<< Updated upstream
-   before_action :configure_permitted_parameters, if: :devise_controller?
-=======
   before_action :configure_permitted_parameters, if: :devise_controller?
->>>>>>> Stashed changes
 
   private
   def configure_permitted_parameters
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,14 @@
 class UsersController < ApplicationController
-
-  def edit
-  end
-<<<<<<< Updated upstream
-  
-def update
-  if current_user.update(user_params)
-    redirect_to root_path
-  else
-    render :edit,status: :unprocessable_entity
-  end
-end
-
-private
-
-def user_params
-  params.require(:user).permit(:name, :email)
-end
-=======
-
   def update
     current_user.update(user_params)
+  end
+  
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit,status: :unprocessable_entity
+    end
   end
 
   private
@@ -28,6 +16,5 @@ end
   def user_params
     params.require(:user).permit(:name, :email)
   end
->>>>>>> Stashed changes
 
 end
